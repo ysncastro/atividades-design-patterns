4278fdda6007f8e9361f54547f2b5509f5540bf6
Yasmin Castro de Souza <yasmin.souza@dbserver.com.br>
2023-03-19 07:11:19 -0300

    exercícios singleton

--- atividades/src/atividades_a_fazer/singleton/Main.java
+++ atividades/src/atividades_a_fazer/singleton/Main.java
@@ -1,8 +1,54 @@
 package atividades_a_fazer.singleton;
 
+class BancoDeDados {
+
+    static Conexao obterConexao() {
+        return new Conexao();
+    }
+
+}
+
+class Conexao {
+
+    public Double id;
+
+    Conexao() {
+        this.id = Math.random();
+    }
+
+    void rodarSQL() {
+        System.out.println("RODANDO SQL" + id);
+    }
+
+}
+class UsuarioDAO {
+
+    void buscarTodos() {
+        System.out.println("buscando todos os usuarios...");
+        BancoDeDados.obterConexao().rodarSQL();
+    }
+
+    void buscarPorId() {
+        System.out.println("buscando usuario por id...");
+        BancoDeDados.obterConexao().rodarSQL();
+    }
+
+    void salvar() {
+        System.out.println("salvando...");
+        BancoDeDados.obterConexao().rodarSQL();
+    }
+
+}
+
 public class Main {
 
     public static void main(String[] args) {
+
+        UsuarioDAO usuarioDAO = new UsuarioDAO();
+
+        usuarioDAO.buscarTodos();
+        usuarioDAO.buscarPorId();
+        usuarioDAO.salvar();
         
     }
 
--- atividades/src/atividades_resolvidas/singleton/Main.java
+++ atividades/src/atividades_resolvidas/singleton/Main.java
@@ -1,9 +1,61 @@
 package atividades_resolvidas.singleton;
 
+class BancoDeDados {
+
+    public static Conexao instanciaConexao;
+
+    public static Conexao obterConexao() {
+        if (instanciaConexao == null) {
+            instanciaConexao = new Conexao();
+        }
+        return instanciaConexao;
+    }
+
+}
+
+class Conexao {
+
+    public Double id;
+
+    Conexao() {
+        this.id = Math.random();
+    }
+
+    void rodarSQL() {
+        System.out.println("RODANDO SQL " + id);
+    }
+
+}
+
+class UsuarioDAO {
+
+    void buscarTodos() {
+        System.out.println("buscando todos os usuarios...");
+        BancoDeDados.obterConexao().rodarSQL();
+    }
+
+    void buscarPorId() {
+        System.out.println("buscando usuario por id...");
+        BancoDeDados.obterConexao().rodarSQL();
+    }
+
+    void salvar() {
+        System.out.println("salvando...");
+        BancoDeDados.obterConexao().rodarSQL();
+    }
+
+}
+
 public class Main {
 
     public static void main(String[] args) {
-        
+
+        UsuarioDAO usuarioDAO = new UsuarioDAO();
+
+        usuarioDAO.buscarTodos();
+        usuarioDAO.buscarPorId();
+        usuarioDAO.salvar();
+
     }
 
 }
