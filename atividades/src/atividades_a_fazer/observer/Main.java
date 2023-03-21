0c1a342838e32f2834b5706bb84d10c7c9053c2a
Yasmin Castro de Souza <yasmin.souza@dbserver.com.br>
2023-03-21 12:50:45 -0300

    removendo mains vazios

--- atividades/src/atividades_a_fazer/observer/Main.java
+++
@@ -1,9 +0,0 @@
-package atividades_a_fazer.observer;
-
-public class Main {
-
-    public static void main(String[] args) {
-        
-    }
-
-}
--- atividades/src/atividades_resolvidas/observer/Main.java
+++
@@ -1,9 +0,0 @@
-package atividades_resolvidas.observer;
-
-public class Main {
-
-    public static void main(String[] args) {
-        
-    }
-
-}
